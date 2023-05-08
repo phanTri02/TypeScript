ce8d5bdd09b08abc11c1a73e854fb3596b0f9715
Trí Phan <phanquoctri1011@gmail.com>
2023-05-09 02:16:53 +0700

    typescript commit

---
+++ features/annotations/functions.ts
@@ -0,0 +1,35 @@
+const add = (a: number , b: number) => {
+  return a + b;
+};
+
+const subtract = (a: number, b: number): number => {
+  return a - b;
+};
+
+function divide(a: number, b: number):number {
+  return a / b;
+};
+
+const multiply = function(a: number, b: number):number {
+  return a * b;
+}
+
+const logger = (message: string): void => {
+  console.log(message);
+};
+
+const throwError = (message: string) => {
+  throw new Error(message)
+};
+
+const todaysWeather = {
+  date: new Date(),
+  weather: 'sunny'
+};
+
+const logWeather = (forecast: { date: Date, weather: string}): void => {
+  console.log(forecast.date);
+  console.log(forecast.weather);
+};
+
+logWeather(todaysWeather);
